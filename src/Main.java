297c646d80f4b65270f7a404c785be99bc35f8bb
JEANLUC ETGEN <doublepunkt@gmail.com>
2013-09-26 15:44:58 +0200

    Test

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: user
+ * Date: 9/26/13
+ * Time: 3:38 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

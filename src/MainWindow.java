6579f1ba713177301c1a90c245947218ab823b8f
JEANLUC ETGEN <doublepunkt@gmail.com>
2013-09-26 21:58:03 +0200

    Framework for Main Window

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: user
@@ -5,6 +10,67 @@
  * Time: 3:39 PM
  * To change this template use File | Settings | File Templates.
  */
-public class MainWindow {
+public class MainWindow extends JFrame{
 
+    private JScrollPane scrollPane = null;
+    private JPanel mainPanel = null;
+
+    private JPanel titlePanel = null;
+    private JLabel title= null;
+    private JTextField titleField = null;
+
+    private JPanel inputPanel = null;
+    private JPanel[] inputPane = new JPanel[6];
+    private JLabel[] paneLabel = new JLabel[6];
+    private JTextArea[] textPane = new JTextArea[6];
+    private String[] paneTitle =
+            {
+            "Materialien",
+            "Vorbereitung",
+            "Aufbau",
+            "Durchführung",
+            "Beobachtung",
+            "Erklärung"
+            };
+
+    public MainWindow(){
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setVisible(true);
+
+        mainPanel = new JPanel(new BorderLayout());
+        titlePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        titleField = new JTextField("Titel", 30);
+        titlePanel.add(titleField);
+        mainPanel.add(titlePanel, BorderLayout.NORTH);
+
+        inputPanel = new JPanel(new GridLayout(6, 1, 10, 20));
+        Border blackBorder = BorderFactory.createLineBorder(Color.BLACK);
+        for (int i = 0; i < 6; i++) {
+            inputPane[i] = new JPanel(new BorderLayout(5, 0));
+            paneLabel[i] = new JLabel(paneTitle[i]);
+            paneLabel[i].setBorder(new EmptyBorder(0, 0, 5, 0));
+            inputPane[i].add(paneLabel[i], BorderLayout.NORTH);
+            textPane[i] = new JTextArea(10, 40);
+            textPane[i].setPreferredSize(new Dimension(490, 160));
+            textPane[i].setBorder(blackBorder);
+            inputPane[i].add(textPane[i], BorderLayout.CENTER);
+            inputPanel.add(inputPane[i]);
+        }
+        mainPanel.add(inputPanel, BorderLayout.SOUTH);
+        mainPanel.setBorder(new EmptyBorder(0, 6, 0, 20));
+
+        scrollPane = new JScrollPane(mainPanel);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+        scrollPane.setPreferredSize(new Dimension(500, 700));
+        scrollPane.setBorder(new EmptyBorder(10, 10, 10, 0));
+
+        this.setPreferredSize(new Dimension(550, 700));
+        getContentPane().add(scrollPane, BorderLayout.CENTER);
+        pack();
+        //System.out.println(this.size());
+        //System.out.println(textPane[0].getHeight());
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: user
@@ -5,9 +8,26 @@
  * Time: 3:36 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Test {
-    public static void main(String[] args) {
-        AufbauCreator ac = new AufbauCreator();
+public class Test extends JPanel {
 
+    private JScrollPane vertical;
+    private JTextArea console;
+
+    public static void main(String[] args) {
+        new JFrame() {{
+            getContentPane().add(new Test());
+            pack();
+            setVisible(true);
+        }};
+    }
+
+
+    public Test() {
+        setPreferredSize(new Dimension(200, 250));
+        console = new JTextArea(15, 15);
+
+        vertical = new JScrollPane(console);
+        vertical.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+        add(vertical);
     }
 }

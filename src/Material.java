3ce91c04f5c0be209564ddd6acc21c93406da0e8
JEANLUC ETGEN <doublepunkt@gmail.com>
2013-09-29 15:32:24 +0200

    Worked on Material Pane

--- src/Material.java
+++ src/Material.java
@@ -1,3 +1,5 @@
+import java.awt.*;
+
 /**
  * Created with IntelliJ IDEA.
  * User: user
@@ -6,4 +8,38 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Material {
+    private float volume;
+    private String name;
+    private Color color;
+
+    public Material(int volume, String name, Color color) {
+        this.volume = volume;
+        this.name = name;
+        this.color = color;
+    }
+
+
+    public float getVolume() {
+        return volume;
+    }
+
+    public void setVolume(float volume) {
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
 }
